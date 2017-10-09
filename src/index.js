f028080ce28d46b1b717cb42ba5d2883b272ff54
kevin power <kevin.power@studiosquared.net>
2017-10-09 19:19:36 -0400

    adjusted sematic config again for codeship build

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 
-import './semantic/dist/semantic.min.css';
+import './semantic/semantic.min.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './index.css';
