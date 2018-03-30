f69d424c2bfb75610e11e632575ed76b5d1050e1
kevin power <kpow@kpow.com>
2018-03-30 00:04:46 -0400

    added IG link

--- src/components/SiteHeader.js
+++ src/components/SiteHeader.js
@@ -1,40 +1,61 @@
 import React, { Component } from 'react';
-import { Container,Menu} from 'semantic-ui-react';
+import { Container, Menu } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 
 import Skull from '../images/skull2.svg';
 
 class SiteHeader extends Component {
   constructor(props) {
-    super(props)
-    this.state = {}
+    super(props);
+    this.state = {};
   }
 
   componentDidMount() {}
 
   render() {
-
     return (
-
       <div>
-      <Menu fixed='top' size='large' inverted compact>
-        <Container>
-            <Menu.Item style={{ padding: '0'}} as={Link} to='/'>
+        <Menu fixed="top" size="large" inverted compact>
+          <Container>
+            <Menu.Item style={{ padding: '0' }} as={Link} to="/">
               <h1 className="site-logo">
-                <img height='50' style={{ display: 'inline-block', paddingTop: '3px', paddingBottom: '5px', verticalAlign: 'middle'  }} src={Skull} />
-                <span style={{ display: 'inline-block', paddingRight: '5px',paddingTop:'5px', height: '50px', verticalAlign:'middle' }}>kpow</span>
+                <img
+                  height="50"
+                  style={{
+                    display: 'inline-block',
+                    paddingTop: '3px',
+                    paddingBottom: '5px',
+                    verticalAlign: 'middle'
+                  }}
+                  src={Skull}
+                />
+                <span
+                  style={{
+                    display: 'inline-block',
+                    paddingRight: '5px',
+                    paddingTop: '5px',
+                    height: '50px',
+                    verticalAlign: 'middle'
+                  }}
+                >
+                  kpow
+                </span>
               </h1>
-        </Menu.Item>
-        <Menu.Item position='right' as={Link} to='/allstars'>allstars</Menu.Item>
-        <Menu.Item  as={Link} to='/allbooks'>allbooks</Menu.Item>
-        <Menu.Item as={Link} to='/resume'>resume</Menu.Item>
-
-        </Container>
-      </Menu>
+            </Menu.Item>
+            <Menu.Item position="right" as={Link} to="/allstars">
+              allstars
+            </Menu.Item>
+            <Menu.Item as={Link} to="/allbooks">
+              allbooks
+            </Menu.Item>
+            <Menu.Item as={Link} to="https://instagram.com/koolyp">
+              insta
+            </Menu.Item>
+          </Container>
+        </Menu>
       </div>
-
-    )
-}
+    );
+  }
 }
 
 export default SiteHeader;
